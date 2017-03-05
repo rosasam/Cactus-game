5e35d7341cfc843408131a34ac29e036cb7e9756
Samuel Rosas <samuel.rosas@aalto.fi>
2017-03-05 02:00:23 +0200

    Change settings value

--- cactusgame.js
+++ cactusgame.js
@@ -65,7 +65,7 @@ $(document).ready(function() {
   // Functions communicating with parent window
 
   // Send window settings
-  window.parent.postMessage({"messageType": "SETTING", "options": {"width": 800, "height": 600} }, "*");
+  window.parent.postMessage({"messageType": "SETTING", "options": {"width": 804, "height": 600} }, "*");
 
   // Send score to parent window
   $("#submit_score").click( function () {
